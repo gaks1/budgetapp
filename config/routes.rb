49944772b7a1879697d7013e17a4e8364fe9f870
isaacg <iwanyoike65@gmail.com>
2023-07-06 13:07:32 +0300

    Fix routes to nest entitties and groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :groups
-  resources :entities
   resources :users
+  resources :groups do
+    resources :entities
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
