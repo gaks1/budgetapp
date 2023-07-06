c24be37adcf1ac2b01f573438cdd505c07e8559f
isaacg <iwanyoike65@gmail.com>
2023-07-06 13:09:44 +0300

    Fix links for entities show page

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
-  has_many :entity_groups
+  has_many :entity_groups, dependent: :destroy
   has_many :groups , through: :entity_groups
 
   validates :name, presence: true, length: { minimum:3, maximum: 255 }
